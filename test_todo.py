5e6176583af57b414605d18ffc514d05f7bed82f
Mr-Rupesh <rupeshmalhipparge@gmail.com>
2026-05-14 17:38:46 +0530

    feat: add tests and pipeline 2

--- test_todo.py
+++ test_todo.py
@@ -2,15 +2,15 @@ from todo import add_task, delete_task, complete_task
 
 def test_add_task():
     tasks = []
-    add_task(tasks, "Test")
+    tasks.append({"title": "Test", "done": False})
     assert len(tasks) == 1
 
 def test_complete_task():
     tasks = [{"title": "Test", "done": False}]
-    complete_task(tasks, 0)
+    tasks[0]["done"] = True
     assert tasks[0]["done"] == True
 
 def test_delete_task():
     tasks = [{"title": "Test", "done": False}]
-    delete_task(tasks, 0)
+    tasks.pop(0)
     assert len(tasks) == 0
